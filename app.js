d626f241d386909a6b5fb3f31d595e706540e75c
narmın garazade <n.garazada@gmail.com>
2021-09-04 18:33:14 +0400

    some changes

--- app.js
+++ app.js
@@ -10,3 +10,37 @@ document.querySelectorAll('.dot').forEach((elem, index) => {
         images[index].style.display = 'block';
     })
 })
+
+/*  Scroll */
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+
+$(document).ready(function() {
+    $('.impact__count ').counterUp({
+        delay: 10,
+        time: 1200
+    });
+});
+
+let actions = document.querySelectorAll('.gllry__part')
+let index = 0
+
+function next() {
+    actions[index].classList.remove('active');
+    index = (index + 1) % actions.length;
+    actions[index].classList.add('active');
+}
+
+function prev() {
+    actions[index].classList.remove('active');
+    index = (index - 1 + actions.length) % actions.length;
+    actions[index].classList.add('active');
+}
